575bc1cae0d07166924cf742179221e4b6b136f4
DJAIR ROBERTO GONÇALVES DA COSTA <djair.costa@ppdf.df.gov.br>
2025-05-29 12:52:44 -0300

    feat: Create Entity

--- src/server.spec.ts
+++
@@ -1,9 +0,0 @@
-import sayHelloWorld from "@/server";
-describe("First test of my life", () => {
-    test("should call sayHelloWorld and return a message", () => {
-        const message = sayHelloWorld ("Hello World!");
-        expect(message).toBe("Hello World!");
-})
-
-
-})
